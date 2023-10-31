f14f5f26910f35037257cf72c3a1259b3f258704
cedTrad <121758643+cedTrad@users.noreply.github.com>
2023-10-31 17:24:06 +0000

    2 agent 1 master

--- magent.py
+++ magent.py
@@ -1,67 +1,10 @@
-from threading import Thread, Lock, Condition, Event
+from threading import Thread
 from system.agent import Agent
 
 
 class MAgentThread(Agent, Thread):
     
-    def __init__(self, agentId, symbol, allocation, env, policy_name, condition, agent_msg="", master_msg="", msg_bus=""):
-        Agent.__init__(self, agentId, symbol, allocation, env, policy_name)
-        Thread.__init__(self)
-        self.agentId = agentId
-        self.condition = condition
-        
-        self.agent_msg = agent_msg
-        self.master_msg = master_msg
-        self.msg_bus = msg_bus
-    
-    
-    def msg_to_master(self, msg):
-        self.agent_msg.append(msg)
-    
-    def get_msg_from_master(self, msg):
-        self.master_msg.append(msg)
-    
-    
-    def wait_msg_from_master(self):
-        while self.master_msg.get(self.agentId) is None:
-            print(f" {self.agentId} wait msg from master ... ")
-            self.condition.wait()
-        
-    def notify_to_master(self):
-        print(f" {self.agentId} send msg to master ... ")
-        self.condition.notify()
-        self.condition.notify_all()
-        
-    
-    def run(self):
-        state = self.env.reset()
-        i = 0
-        while True:
-            with self.condition:
-                
-                self.wait_msg_from_master()                                
-                try:
-                    next_state, reward, event = self.update(state)
-                    state = next_state
-                    i += 1
-                except StopIteration:
-                    break
-                    
-                trades_data = self.env.journal.trades_data.copy()
-                if "Close" in self.asset.state:
-                    self.post_trade(event=event, trades_data = trades_data, close_trade=True)
-                    
-                self.agent_msg[self.Id] = "order send ..."
-                print(f"agent {self.Id} place order successfully ... ")
-                self.condition.notify()
-            
-        
-    
-
-
-class MAgentThread2(Agent, Thread):
-    
-    def __init__(self, agentId, symbol, allocation, env, policy_name, condition, agent_msg="", master_msg="", barrier=""):
+    def __init__(self, agentId, symbol, allocation, env, policy_name, condition, agent_msg, master_msg, barrier):
         Agent.__init__(self, agentId, symbol, allocation, env, policy_name)
         Thread.__init__(self)
         self.agentId = agentId
@@ -78,19 +21,23 @@ class MAgentThread2(Agent, Thread):
         i = 0
         while True:    
             with self.condition:
-                
                 while self.master_msg.get(self.agentId) is None:
                     print(f"{self.agentId} en attente des ordres du master ... ")
                     self.condition.wait()
                 
                 print(f"{self.agentId} en cours d'execution ... ")
-                get_msg = self.master_msg.get.pop(self.agentId)
+                print(f"======> {self.master_msg.get(self.agentId)}")
+                boss_msg = self.master_msg.pop(self.agentId, None)
+                print(f"___ msg : {boss_msg}")
                 
                 try:
                     next_state, reward, event = self.update(state)
                     state = next_state
                     i += 1
                 except StopIteration:
+                    self.agent_msg.update({self.agentId : "stop"})
+                    print(f"---------- STOP {self.agentId}----------")
+                    self.condition.notify()
                     break
                         
                 trades_data = self.env.journal.trades_data.copy()
@@ -98,7 +45,11 @@ class MAgentThread2(Agent, Thread):
                     self.post_trade(event=event, trades_data = trades_data, close_trade=True)
                 print(f"{self.agentId} - date : {event.date}, {self.symbol}")
                 
+                self.agent_msg.update({self.agentId : "order placed ... "})
+                
                 self.condition.notify()
             
             self.barrier.wait()
-            
+        
+        
+
--- master.py
+++ master.py
@@ -3,7 +3,7 @@ from threading import Thread, Condition, Event
 
 class MasterAgentThread(Thread):
     
-    def __init__(self, condition, activeAgent="", agent_msg="", master_msg=""):
+    def __init__(self, condition, activeAgent, agent_msg, master_msg):
         Thread.__init__(self)
         self.activeAgent = activeAgent
         self.agentList = []
@@ -21,16 +21,26 @@ class MasterAgentThread(Thread):
     def run(self):
         while True:
             with self.condition:
+                print("**** Master ****")
                 for agentId in self.agentList:
                     self.master_msg.update({agentId : "Place an Order ... "})
                 self.condition.notify_all()
                 print("Master a tous les agents , Activez-vous")
                 
+                
                 while all([msg for msg in self.agent_msg.values()]) is False:
                     print("Waiting for agents ... ")
                     self.condition.wait()
                 
-                self.agent_msg.clear()
+                print(f" ooo__oooo=> : {self.agent_msg}")
+                stop_condition = self.agent_msg == {self.agentList[0] : "stop", self.agentList[1] : "stop"}
+                print(f"stop condition : {stop_condition}")
+                if stop_condition:
+                    print("---------- STOP(Boss) ----------")
+                    break
+                    
+                
+                self.agent_msg.update({self.agentList[0] : None, self.agentList[1] : None})
                 
                 
     def global_report(self):
