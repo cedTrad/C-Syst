e32d86ef392ec600a37d72b138fd986f7db9bb48
cedricPma <cedric.tiendrebeogo@pma2020.org>
2023-12-12 23:35:17 +0000

    change variable name ...msg to ...bus, and structure

--- magent.py
+++ magent.py
@@ -4,49 +4,66 @@ from system.agent import Agent
 
 class MAgentThread(Agent, Thread):
     
-    def __init__(self, agentId, symbol, allocation, env, policy_name, condition, agent_msg, master_msg, barrier):
+    def __init__(self, agentId, symbol, allocation, env, policy_name, condition, agent_bus, master_bus, barrier):
         Agent.__init__(self, agentId, symbol, allocation, env, policy_name)
         Thread.__init__(self)
         self.agentId = agentId
         self.condition = condition
         
-        self.agent_msg = agent_msg
-        self.master_msg = master_msg
+        self.agent_bus = agent_bus
+        self.master_bus = master_bus
         
         self.barrier = barrier
+    
+    
+    def is_active(self, master_bus):
+        return master_bus["activeAgent"] == self.agentId
+    
+    
+    def start_work(self):
+        ""
         
+    def end_work(self, agent_bus):
+        agent_bus
+    
     
     def run(self):
         state = self.env.reset()
-        i = 0
+        
         while True:    
             with self.condition:
-                while self.master_msg.get(self.agentId) is None:
+                while self.master_bus["agent"].get(self.agentId) is None:
                     print(f"{self.agentId} waiting master's signal ... ")
                     self.condition.wait()
                 
                 print(f"{self.agentId} get master signal ... ")
-                boss_msg = self.master_msg.pop(self.agentId, None)
-                print(f"{self.agentId}- Master signal : {boss_msg}")
+                boss_msg = self.master_bus["agent"].pop(self.agentId, None)
+                print(f"{self.agentId} - Master signal : {boss_msg}")
                 
                 try:
-                    next_state, reward, event = self.update(state)
-                    state = next_state
-                    i += 1
+                    if self.is_active(self.master_bus):
+                        next_state, reward, event = self.update(state)
+                        state = next_state
+                        print("date : ",event.date)
+
                 except StopIteration:
-                    self.agent_msg.update({self.agentId : "stop"})
+                    #self.agent_bus["msg"].update({self.agentId : "stop"})
+                    self.agent_bus.update({"stop" : True})
                     print(f"---------- STOP {self.agentId}----------")
+                    stop = True
+                    self.master_bus.update({"stop" : True}) 
                     self.condition.notify()
+                    
                     break
-                        
-                        
-                print(f"{self.agentId} - date : {event.date}, {self.symbol}")
                 
-                self.agent_msg.update({self.agentId : "action executed ... "})
-                
+                self.agent_bus["msg"].update({self.agentId : "action executed ... "})
                 self.condition.notify()
             
             self.barrier.wait()
+            
+            if self.agent_bus.get("stop") is True:
+                print(f"{self.agentId} STOP")
+                break
         
         
 
--- master.py
+++ master.py
@@ -3,20 +3,26 @@ from threading import Thread, Condition, Event
 
 class MasterAgentThread(Thread):
     
-    def __init__(self, condition, activeAgent, agent_msg, master_msg):
+    def __init__(self, condition, activeAgent, agent_bus, master_bus):
         Thread.__init__(self)
         self.activeAgent = activeAgent
         self.agentList = []
         
         self.condition = condition
         
-        self.agent_msg = agent_msg
-        self.master_msg = master_msg
+        self.agent_bus = agent_bus
+        self.master_bus = master_bus
     
     
     def addAgent(self, agentId):
         self.agentList.append(agentId)
     
+    
+    def is_all_running(self, agent_bus):
+        msg = agent_bus["running"].values()
+        return all(msg)
+        
+        
     def fitness(self):
         ""
     
@@ -25,22 +31,27 @@ class MasterAgentThread(Thread):
             with self.condition:
                 print("**** Master ****")
                 for agentId in self.agentList:
-                    self.master_msg.update({agentId : "Place an Order ... "})
-                self.condition.notify_all()
-                print("Let's go ... ")
+                    self.master_bus["agent"].update({agentId : "Start work ..."})
                 
-                while all([msg for msg in self.agent_msg.values()]) is False:
+                self.master_bus.update({"activeAgent" : 1})
+                self.condition.notify_all()
+                
+                print("Let's go ... ")
+                print(f"{self.master_bus}")
+                
+                #while all([msg for msg in self.agent_bus["msg"].values()]) is False:
+                while self.is_running(self.agent_bus):
                     print("Master are waiting for agents ... ")
-                    print(f"{self.agent_msg}")
+                    print(f"{self.agent_bus}")
                     self.condition.wait()
                 
-                print(f" tracking ... : {self.agent_msg}")
-                stop_condition = self.agent_msg == {agent : "stop" for agent in self.agentList}
+                stop_condition = 'stop' in self.agent_bus["msg"].values()
+                print(f"Stop condition : {stop_condition}")
                 if stop_condition:
                     print("---------- STOP(Boss) ----------")
                     break
                 
-                self.agent_msg.update(
+                self.agent_bus["msg"].update(
                     {agent : None for agent in self.agentList}
                 )
                 
